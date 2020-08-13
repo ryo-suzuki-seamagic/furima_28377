a59f4e4bd34407ee0aaa4f45d2b21d6550ed3e4c
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-13 18:18:11 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,24 @@ class User < ApplicationRecord
       has_many :comments
       has_many :purchases
 
+      #バリデーション
+
+  #ニックネームが必須であること
+  validates :name, presensce: true
+  #メールアドレスが必須であること
+  validates :email, presensce: true
+  #メールアドレスが一意性であること
+  validates :email,  uniqueness: true
+  #メールアドレスは@を含む必要があること
+  validates :email, inclusion: { in: '@'}
+  #パスワードが必須であること
+  validates :password,  presemsce: true
+  #パスワードは6文字以上であること
+  validates :password, length: { minimum: 5 }
+  #パスワードは半角英数字混合であること
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.?\d)(?=.*?\w)[!-~]+\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  #パスワードは確認用を含めて2回入力すること
+  validates :password, confirmation: true
+
 end

b3eefabf860370361fb0bb120e98937d5af74eaf
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-18 16:59:22 +0900

    バリデーションremake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
   private
 
   def basic_auth
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:name,:email,:password,:birthday,:first_name,:family_name,:first_name_kana,:family_name_kana])
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :email, :password, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
     end
   
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,30 +10,21 @@ class User < ApplicationRecord
 
       #バリデーション
 
-# ニックネームが必須であること
-# メールアドレスが必須であること
-# メールアドレスが一意性であること
-# メールアドレスは@を含む必要があること
-# パスワードが必須であること
-# パスワードは6文字以上であること
-# パスワードは半角英数字混合であること
-# パスワードは確認用を含めて2回入力すること
-# ユーザー本名が、名字と名前がそれぞれ必須であること
-# ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること
-# ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること
-# ユーザー本名のフリガナは全角（カタカナ）で入力させること
-# 生年月日が必須であること
+
+
 
       with_options presence: true do
         validates :nickname
         validates :email, uniqueness: true,
-                    inclusion: { in: '@'},
-                    format: { with: /\A([a-zA-Z0-9]{6,})\z/ },
+                  confirmation: true
+        validates :password,
+                    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/},
                     confirmation: true
-        validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+                     
         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-        validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/}
+        validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
         validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/}
+        validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/}
         validates :birthday
       end
 end

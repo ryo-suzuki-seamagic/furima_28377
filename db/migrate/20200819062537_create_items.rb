489934e391cdcdb075240ed5621fadb359e3e022
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-19 16:28:37 +0900

    itemsテーブルのカラム設定

--- db/migrate/20200819062537_create_items.rb
+++ db/migrate/20200819062537_create_items.rb
@@ -2,6 +2,16 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
+      
+      t.string :title,              null: false
+      t.text :text,                 null: false
+      t.integer :kategory_id,       null: false
+      t.integer :stasus_id,         null: false
+      t.integer :delivary_fee_id,   null: false
+      t.integer :from_id,           null: false
+      t.integer :days_id,           null: false
+      t.integer :price,             null: false
+      t.references :user,           null: false, foreign_key: true
       t.timestamps
     end
   end

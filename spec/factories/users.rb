3aaf3f0293d7a48fbec485ad3040448c8ccb2c36
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-19 12:33:51 +0900

    単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname                                {Faker::Name.initials}
+    email                                   {Faker::Internet.free_email}
+    password                                {"pass0000"}
+    password_confirmation                   {password}
+    birthday                                {Faker::Date.birthday}
+    first_name                               {"鈴木"}
+    last_name                                {"諒"}
+    first_name_kana                          {"スズキ"}
+    last_name_kana                           {"リョウ"}
   end
 end

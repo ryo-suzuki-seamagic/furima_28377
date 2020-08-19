9a967adc7d339bb4991f7214869a2a15d149e535
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-19 12:37:17 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-      has_many :items
-      has_many :comments
-      has_many :purchases
+  has_many :items
+  has_many :comments
+  has_many :purchases
 
-      #バリデーション
+  # バリデーション
 
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true,
+                      confirmation: true
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ },
+              confirmation: true
 
-
-
-      with_options presence: true do
-        validates :nickname
-        validates :email, uniqueness: true,
-                  confirmation: true
-        validates :password,
-                    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/},
-                    confirmation: true
-                     
-        validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-        validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-        validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/}
-        validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/}
-        validates :birthday
-      end
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
 end

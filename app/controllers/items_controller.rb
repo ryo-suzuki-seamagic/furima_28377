270e6f71af683bdc8a25f9feb212f355e9d58217
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-20 18:01:45 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, notice:'出品できません'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :text, :kategory_id, :status_id, :delivery_fee_id, :from_id, :day_id, :price, :user)
   end
 end

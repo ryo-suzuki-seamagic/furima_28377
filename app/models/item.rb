1b60eb03294c145c3d7b3f8ed6b302839a288f8b
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-20 18:02:11 +0900

    パス指定、エラ〜メッセージ表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,33 @@ class Item < ApplicationRecord
    belongs_to_active_hash :day
    belongs_to_active_hash :from
 
-   
+   #バリデーション
+
+  
+# 商品名が必須であること
+# 商品の説明が必須であること
+# カテゴリーの情報が必須であること
+# 商品の状態についての情報が必須であること
+# 配送料の負担についての情報が必須であること
+# 発送元の地域についての情報が必須であること
+# 発送までの日数についての情報が必須であること
+# 価格についての情報が必須であること
+# 価格の範囲が、¥300~¥9,999,999の間であること
+
+    with_options presence: true do
+      validates :title
+      validates :text
+      validates :kategory
+      validates :status
+      validates :delivery_fee
+      validates :from
+      validates :day
+      validates :price, format:{ with:/\A[300 - 999999]+\z/  }
+    
+    
+
+    end
+
+
 
 end

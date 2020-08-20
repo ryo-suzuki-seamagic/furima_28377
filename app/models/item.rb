c76ff2898d5020305409d5c039a9746a37b196ee
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-20 10:46:25 +0900

    アソシエーション remake

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to :user
    has_many :comments
    has_one :purchase
    has_one_attached :image
-
+   belongs_to_active_hash :kategory
+   belongs_to_active_hash :status
+   belongs_to_active_hash :delivery_fee
+   belongs_to_active_hash :day
+   belongs_to_active_hash :from
 
    
 

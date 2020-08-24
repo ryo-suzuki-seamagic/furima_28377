c286f71be21b1b9bc02b90cf096aaa832e62f638
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-24 11:43:03 +0900

    テストコード　FActoryBot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    image                           {Faker::Lorem.sentens}
+    title                           {Faker::title}
+    text                            {Faker::text}
+    kategory_id                     {Faker::number::(2-11)}
+    status_id                       {Faker::number::(2-7)}
+    delivery_fee_id                 {Faker::number::(2-3)}
+    from_id                         {Faker::number::(2-48)}
+    day_id                          {Faker::number::(2-4)}
+    price                           {Faker::number::(300-9999999)}
+    association :user
+
   end
 end

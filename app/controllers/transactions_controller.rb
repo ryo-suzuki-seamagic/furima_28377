40ec7b0e1beddfd289ec1033af5c87eb0f816d5a
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-27 19:01:46 +0900

    アクション設定

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,17 @@
 class TransactionsController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :text, :kategory_id, :status_id, :delivery_fee_id, :from_id, :day_id, :price).merge(user_id: current_user.id)
   end
 end
+

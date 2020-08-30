6fa98154f10547aa472e6855dfbb11d419512a59
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-08-30 17:58:12 +0900

    購入アクション定義
    pay.jp導入

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,24 +7,19 @@ class TransactionsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
-  # def new
-  #   @purchase = PurchaseAddress.new 
-  # end
 
   def create
     
     @purchase = PurchaseAddress.new(purchase_params)
     if  @purchase.valid?
-      pay_item
+        pay_item
         @purchase.save
       return redirect_to root_path
     else
       render "index"
     end
-
+ 
   end
-  
-  private
 
   def item_user
     @item = Item.find(params[:item_id])
@@ -33,14 +28,17 @@ class TransactionsController < ApplicationController
       end
   end
 
+  private
+
     def purchase_params
-      params.require(:purchase_address).permit(:postal_code, :prefecure_id, :muncipalities, :address, :building_name, :phone_number, :price, :token )
+      params.permit(:postal_code, :prefecure_id, :muncipalities, :address, :building_name, :phone_number, :price, :item_id, :token).merge(user_id: current_user.id)
     end
 
     def pay_item
-      Payjp.api_key = 
+      @item = Item.find(params[:item_id])
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        amount: purchase_params[:price],
+        amount: @item.price,
         card: purchase_params[:token],
         currency:'jpy'
       )
--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecure_id, :muncipalities, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecure_id, :muncipalities, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
 
   with_options presence: true do
@@ -12,7 +12,7 @@ class PurchaseAddress
     validates :prefecure_id, numericality: { other_than: 1, message: "Select"}
 
     def save
-      purchase = Purchase.create(user_id: user.id, item_id: item.id)
-      Address.create(postal_code: postal_code, prefecure_id: prefecure_id, muncipalities: muncipalities, address: address, building_name: building_name, phone_number: phone_number)
+      purchase = Purchase.create(user_id: user_id, item_id: item_id)
+      Address.create(purchase_id: purchase.id, postal_code: postal_code, prefecure_id: prefecure_id, muncipalities: muncipalities, address: address, building_name: building_name, phone_number: phone_number)
     end
 end
